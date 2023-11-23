55056d3a92973362d58257cf1ddd9e648812ac16
Alejo Herrera <alejoherrera1166@gmail.com>
2023-11-23 06:48:02 -0300

    Tarea como agregar elementos a la lista terminado.

--- js/script.js
+++ js/script.js
@@ -1,4 +1,30 @@
 /*
-1- Crea una web con bootstrap y js, que contenga un botón comenzar el juego, en ese momento se crea un número aleatorio que el usuario deberá adivinar, la interfaz del usuario debe tener además un input para ingresar un número y un botón enviar, al presionar el botón enviar mostrar en un alert si el usuario adivino o no el número mágico, si no lo adivino indicarle con un alert si el numero que ingreso es mayor o menor al número mágico.
-Cuando el usuario adivine el numero mostrar un mensaje indicando al usuario que adivino el numero.
+3 - Crea una web con bootstrap y js, que contenga un botón input donde se pueda cargar una tarea y un botón que al ser presionado agregue dicha tarea a una lista, cada elemento ingresado en la lista debe poder ser eliminado con un botón creado para ese fin. 
 */
+const agregar = (e) => {
+    e.preventDefault();
+
+    const cadena = document.getElementById("agregar");
+    
+    const tareaNueva = document.createElement("li");
+    tareaNueva.innerHTML = cadena.value;
+
+    const seccionInformacionExtra = document.getElementsByClassName("lista");
+    seccionInformacionExtra[0].insertBefore(tareaNueva,primera);
+
+    console.log(seccionInformacionExtra[0].children);
+}
+
+const eliminar = (e) => {
+    e.preventDefault();
+
+    const cadena = document.getElementById("borrar");
+
+    console.log("Estoy en la función borrar.");
+}
+
+const formAgregar = document.getElementById("formAgregar");
+const formEliminar = document.getElementById("formEliminar");
+
+formAgregar.addEventListener("submit",agregar);
+formEliminar.addEventListener("submit",eliminar);
